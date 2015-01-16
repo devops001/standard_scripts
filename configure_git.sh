9b1171bb08f8e80775666ff3d01da941418dbfee
devops001 <devops001@gmail.com>
2015-01-16 15:01:38 -0800

    simple git setup

---
+++ configure_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name   devops001
+git config --global user.email  devops001@gmail.com
+git config --global core.editor vi
+git config --global color.ui    true
+
